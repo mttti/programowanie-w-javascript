0e92763ed109f6a9778364d0b754315b04d6d8ed
Mateusz <mateusz.magiera010@gmail.com>
2023-12-11 00:19:04 +0100

    sledzik

--- lab06/main.js
+++ lab06/main.js
@@ -1,53 +1,50 @@
 let marginTop;
-let marginLeft; 
+let marginLeft;
 
-window.addEventListener('deviceorientation', onDeviceMove)
+window.addEventListener("deviceorientation", onDeviceMove);
 
 function onDeviceMove(event) {
-    //console.log(event.beta)
-    //console.log(event.gamma)
-    moveBall(event.beta, event.gamma)
+    console.log(event.beta);
+    moveBall(event.beta, event.gamma);
 }
 
-function animate() {
-       //console.log(Date.now())
-    requestAnimationFrame(animate)
-}
+// function animate() {
+//     requestAnimationFrame(animate);
+// }
 
-requestAnimationFrame(animate)
+// requestAnimationFrame(animate);
 
-function randomHoleLocation(){//max 330
-    marginTop = Math.floor(Math.random() * 330 );
-    marginLeft = Math.floor(Math.random() * 330 );
+function randomHoleLocation() {
+    //max 330
+    marginTop = Math.floor(Math.random() * 330);
+    marginLeft = Math.floor(Math.random() * 330);
 
     const hole = document.querySelector("#dolek");
-    console.log(hole)
     hole.style.marginTop = `${marginTop}px`;
     hole.style.marginLeft = `${marginLeft}px`;
-    //hole.style.transform=`translate(${marginLeft}, ${marginTop})`;
-    console.log(marginLeft)
 }
 
 // function getHoleLocation(){
 //     const hole = document.querySelector("#dolek");
 // }
 
-
 //BETA: + W DÓL - W GÓRE max 180
 //GAMMA: + W PRAWO - W LEWO max 90
-function moveBall(beta, gamma){
+function moveBall(beta, gamma) {
+    if (beta > 90) {
+        beta = 90;
+    }
+    if (beta < -90) {
+        beta = 90;
+    }
+    console.log(beta);
+    beta += 90;
+    gamma += 90;
+
     const ball = document.querySelector("#pilka");
 
-    ball.style.top = `${beta*2}px`
-    ball.style.left = `${gamma*3}px`
-
-    if(beta*2>marginTop && beta*2<marginTop+70 && gamma*3>marginLeft&&gamma*3<marginLeft+70){
-        alert("Wygrałeś")
-        ball.style.top="0px";
-        ball.style.left="0px";
-        randomHoleLocation();
-    }
+    ball.style.marginLeft = `${(360 * gamma) / 180 - 5}px`;
+    ball.style.marginTop = `${(360 * beta) / 180 - 5}px`;
 }
 
-
-randomHoleLocation();   
+randomHoleLocation();
--- lab08/main.js
+++ lab08/main.js
@@ -1,39 +1,118 @@
 const API_KEY = "f44eef0ba916c40deac3b3810745abaa";
 
-const findCityButton = document.querySelector("#findCity");
+const addCityButton = document.querySelector("#findCity");
+const deleteInfoButton = document.querySelectorAll(".delete");
 
-async function getCityLocation(city) {
-  const response = await fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${API_KEY}`
-  );
-  let cords = (await response.json())[0];
-  let lat = cords.lat;
-  let lon = cords.lon;
-
-  getWeatherForCity(lat, lon);
+async function listCities() {
+    for (let index = 0; index < localStorage.length; index++) {
+        const cityInfo = JSON.parse(
+            localStorage.getItem(localStorage.key(index))
+        );
+        await addCity(cityInfo.name);
+        //addWeatherInfo(cityInfo);
+    }
 }
 
-async function getWeatherForCity(lat, lon) {
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&exclude=hourly,daily&appid=${API_KEY}`
-  );
+listCities();
 
-  let data = await response.json();
-  let temp = data.main.temp;
-  let humidity = data.main.humidity;
-  let icon = data.weather[0].icon;
-  console.log(icon);
+async function getWeatherForCity(cityName) {
+    const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
+    );
+    let data = await response.json();
+    let date = new Date(data.dt * 1000);
 
-  console.log(data);
+    let hours =
+        date.getHours().toString().length < 2
+            ? 0 + date.getHours().toString()
+            : date.getHours();
+
+    let minutes =
+        date.getMinutes().toString().length < 2
+            ? 0 + date.getMinutes().toString()
+            : date.getMinutes();
+
+    let dateTime = `${hours}:${minutes}`;
+
+    let temp = data.main.temp;
+    let humidity = data.main.humidity;
+    let iconId = data.weather[0].icon;
+    let info = {
+        name: cityName,
+        temp: temp,
+        humidity: humidity,
+        iconId: iconId,
+        time: dateTime,
+    };
+
+    return info;
 }
 
 function getIcon(id) {
-  let iconSrc = `https://openweathermap.org/img/wn/${id}@2x.png`;
+    let iconSrc = `https://openweathermap.org/img/wn/${id}@2x.png`;
+    return iconSrc;
 }
 
-findCityButton.addEventListener("click", () => {
-  let input = document.querySelector("#city");
-  let city = input.value;
+async function addCityButtonHandler() {
+    let input = document.querySelector("#city");
+    let city = input.value;
 
-  getCityLocation(city);
-});
+    if (localStorage.getItem(city) !== null) {
+        alert("To miasto zostało już dodane");
+        return;
+    }
+    if (localStorage.length >= 10) {
+        alert("Maksymalna liczba miast do śledzenia to 10!");
+        input.disabled = true;
+    }
+
+    await addCity(city);
+}
+
+async function addCity(city) {
+    try {
+        let info = await getWeatherForCity(city);
+        addWeatherInfo(info);
+    } catch (error) {
+        alert("Wystąpił błąd - sprawdź konsole");
+        console.log(error);
+    }
+}
+
+function addWeatherInfo(info) {
+    const main = document.querySelector("#main");
+    const newInfo = document.createElement("div");
+    const url = getIcon(info.iconId);
+    const cityName = info.name[0].toUpperCase() + info.name.slice(1);
+
+    window.localStorage.setItem(cityName, JSON.stringify(info));
+
+    const deleteButton = document.createElement("p");
+    deleteButton.className = "delete";
+    deleteButton.textContent = "Usuń";
+
+    newInfo.className = "info";
+    const infoInnerHtml = `
+        <h1> ${cityName}</h1>
+        <img src=${url}>
+        <p> Temperatura: ${(info.temp - 273.15).toFixed(1)}C</p>
+        <p>Wilgotność powietrza: ${info.humidity} </p>
+        
+        <p class="time">Godzina aktualizacji: ${info.time}</p>
+        `;
+
+    newInfo.innerHTML = infoInnerHtml;
+    newInfo.appendChild(deleteButton);
+    main.appendChild(newInfo);
+
+    deleteButton.addEventListener("click", () => {
+        newInfo.remove();
+        localStorage.removeItem(cityName);
+    });
+}
+
+addCityButton.addEventListener("click", addCityButtonHandler);
+
+// deleteInfoButton.addEventListener("click", () => {
+//     console.log("xf");
+// });
