effb57de043d2e3d1768fadbd70eab84d99889db
Mateusz <mateusz.magiera010@gmail.com>
2023-12-14 23:32:03 +0100

    lab04 tags

--- lab04/main.js
+++ lab04/main.js
@@ -1,43 +1,58 @@
 const addButton = document.querySelector("#addNoteButton");
 const deleteAllNotesButton = document.querySelector("#deleteAllNotes");
+const selectByTagButton = document.querySelector("#selectByTagButton");
+const selectAllButton = document.querySelector("#selectAll");
 const defaultColor = "yellow";
 
-
-function listNotes(){
-    for (let index = 0; index < localStorage.length; index++) {      
+function listNotes(tag = null) {
+    for (let index = 0; index < localStorage.length; index++) {
         const note = JSON.parse(localStorage.getItem(index));
-        createNote(note);
+        if (!tag) {
+            createNote(note);
+        } else {
+            if (note.tags.includes(tag)) {
+                createNote(note);
+            }
+        }
     }
 }
+if (localStorage.length > 0) {
+    listNotes();
+}
 
-listNotes();
-function createNote(note){
+function createNote(note) {
     const allNotes = document.querySelector("#notes");
-    const newNote = document.createElement("div")
-    newNote.className="note";
+    const newNote = document.createElement("div");
+    newNote.className = "note";
+
     newNote.style.backgroundColor = note.color;
     const noteInnerHtml = `
     <h1> ${note.title}</h1>
     <p> ${note.content}<p>
     <p>Data dodania: ${note.date}</p>
+    <p class="tags">${note.tags.map((tag) => ` #${tag.trim()}`)}</p>
     `;
-    newNote.innerHTML=noteInnerHtml;
+    newNote.innerHTML = noteInnerHtml;
 
-    if(note.isPinned){
-        allNotes.insertBefore(newNote, allNotes.firstChild)
-    }else{
+    if (note.isPinned) {
+        allNotes.insertBefore(newNote, allNotes.firstChild);
+    } else {
         allNotes.appendChild(newNote);
     }
 }
 
-function addNote(){
+function addNote() {
     const title = document.querySelector("#title").value;
     const content = document.querySelector("#content").value;
-    const noteColor = document.querySelector("#noteColor").value === ''? defaultColor:document.querySelector("#noteColor").value;
+    const noteColor =
+        document.querySelector("#noteColor").value === ""
+            ? defaultColor
+            : document.querySelector("#noteColor").value;
+    const tags = document.querySelector("#tagsInput").value.split(",");
     const isPinned = document.querySelector("#pin").checked;
     const id = localStorage.length;
 
-    if(title===''|| content===''){
+    if (title === "" || content === "" || tags[0] === "") {
         alert("Uzupełnij pola");
         return;
     }
@@ -45,25 +60,41 @@ function addNote(){
     const dateNow = new Date();
     const date = `${dateNow.getDay()}/${dateNow.getMonth()}/${dateNow.getFullYear()} ${dateNow.getHours()}:${dateNow.getMinutes()}`;
 
-    const note ={
+    const note = {
         id: id,
         title: title,
         content: content,
+        tags: tags,
         color: noteColor,
         isPinned: isPinned,
-        date: date
+        date: date,
     };
- 
-    createNote(note)
-    window.localStorage.setItem(note.id, JSON.stringify(note));
 
+    console.log(note);
+    createNote(note);
+    window.localStorage.setItem(note.id, JSON.stringify(note));
 }
 
-function deleteAllNotes(){
+function deleteAllNotes() {
     localStorage.clear();
     const allNotes = document.querySelector("#notes");
-    allNotes.innerHTML='';
+    allNotes.innerHTML = "";
+}
+
+function selectByTag() {
+    const tag = document.querySelector("#selectByTag").value;
+    const allNotes = document.querySelector("#notes");
+    allNotes.innerHTML = "";
+
+    listNotes(tag);
 }
 
 addButton.addEventListener("click", addNote);
 deleteAllNotesButton.addEventListener("click", deleteAllNotes);
+selectByTagButton.addEventListener("click", selectByTag);
+selectAllButton.addEventListener("click", () => {
+    const allNotes = document.querySelector("#notes");
+    allNotes.innerHTML = "";
+
+    listNotes();
+});
